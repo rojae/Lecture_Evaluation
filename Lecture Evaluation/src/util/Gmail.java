fc8362f62bb2bff05e724d5ebec34760407e2e48
rojae <41769568+rojae@users.noreply.github.com>
2020-03-09 02:54:17 +0900

    Update Gmail.java

--- Lecture Evaluation/src/util/Gmail.java
+++ Lecture Evaluation/src/util/Gmail.java
@@ -6,6 +6,6 @@ import javax.mail.PasswordAuthentication;
 public class Gmail extends Authenticator{
 	@Override
 	protected PasswordAuthentication getPasswordAuthentication() {
-		return new PasswordAuthentication("jaethon96@gmail.com","castropollux0249");
+		return new PasswordAuthentication("mail","password");
 	}
 }
